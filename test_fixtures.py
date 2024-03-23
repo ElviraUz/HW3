3ec38a9b60da1a8f011f25c6ca26b4147d866031
Elvira Uzlova <e.uzlovaia@gmail.com>
2024-03-23 16:02:31 +0100

    fixed mistakes

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,22 +1,25 @@
 import pytest
 from selene import browser, be, have
 
+
 @pytest.fixture(scope="session")
 def browser_settings():
     browser.config.base_url = 'https://google.com'
-    browser.driver.set_window_size(1920,1080)
+    browser.driver.set_window_size(1920, 1080)
     yield
     browser.config.timeout = 9000.0
     browser.quit()
     print("Закрываем браузер")
 
+
 def test_positive(browser_settings):
     browser.open('/')
     button = browser.element('#L2AGLb')
-    if button.should(be.visible):
+    if button.matching(be.visible):
         button.click()
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_negative(browser_settings):
     browser.open('/')
